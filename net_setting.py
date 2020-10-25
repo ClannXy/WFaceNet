6047117f80274e39f126e2d854277c0a6448cad1
clannxy <clannxy@gmail.com>
2020-10-26 02:07:41 +0800

    Update net_setting.py
    delete unnecessary configuration.

--- net_setting.py
+++ net_setting.py
@@ -16,24 +16,6 @@ WFaceNet_112X112 = [
 # flops: 338.071456m
 # ave: 99.3500%  47E,64E,66E
 
-WFaceNet_112X112_hs = [  
-    # oup, exp, k, s,    SE,      NL
-    [ 32,  64,  3, 2,  True, "hswish" ],    
-    [ 64,  128, 3, 2, False, "hswish" ],   
-    [ 64,  128, 3, 1, False, "hswish" ],
-    [128,  256, 3, 2,  True, "hswish" ],   
-    [128,  256, 3, 1,  True, "hswish" ],
-    [128,  256, 3, 1,  True, "hswish" ], 
-    [128,  256, 3, 1,  True, "hswish" ],
-    [128,  256, 3, 1,  True, "hswish" ],
-    [128,  512, 3, 2,  True, "hswish" ],   
-    [128,  256, 3, 1,  True, "hswish" ],
-    [128,  256, 3, 1,  True, "hswish" ],
-]
-# flops: 338.071456m
-# params: 1.163392 m
-# ave: 99.3500%  47E,64E,66E
-
 MobileFaceNet_Setting= [ 
     # oup, exp, k, s,    SE,      NL
     [  64, 128, 3, 2, False,  "prelu" ],
@@ -53,6 +35,7 @@ MobileFaceNet_Setting= [
     [ 128, 256, 3, 1, False,  "prelu" ],
 ]
 # paramerters: 995104.
+# ave: 99.28%
 
 MOBILENET_V3_SMALL_BOTTLENECK_SETTING= [    
     # oup, exp, k, s,    SE,      NL
@@ -87,24 +70,6 @@ Test_Setting_21= [
 # params: 1200256.
 # ave: 99.3167%
 
-Test_Setting_33 = [  
-    # oup, exp, k, s,    SE,      NL
-    [ 32,  64, 3, 2,  False, "prelu" ],   
-    [ 64,  128, 3, 2, False, "prelu" ],    
-    [ 64,  128, 3, 1, False, "prelu" ],
-    [128,  256, 3, 2, False, "prelu" ],   
-    [128,  256, 3, 1, False, "prelu" ],
-    [128,  256, 3, 1, False, "prelu" ], 
-    [128,  256, 3, 1, False, "prelu" ],
-    [128,  256, 3, 1, False, "prelu" ],
-    [128,  512, 3, 2, False, "prelu" ],   
-    [128,  256, 3, 1, False, "prelu" ],
-    [128,  256, 3, 1, False, "prelu" ],
-]
-# paramerters: 806688.
-# flops: 336.338208m
-# Epoch 65: 99.13%
-
 Min_Setting = [
     # oup, exp, k, s,    SE,      NL
     [ 32,  64, 3, 2,  False, "prelu" ],   
@@ -112,8 +77,6 @@ Min_Setting = [
     [ 64,  128, 3, 2, False, "prelu" ],
 ]
 # paramerters: 163872.
-
-
 
 # current 
 # 当前网络调用的
